4245e19848d5b481632b535cd81d372048e9a3bc
kingpro068 <34006631+kingpro068@users.noreply.github.com>
2018-06-20 01:08:42 +0700

    Add files via upload

---
+++ Function.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#include "MasterHeader.h"
+
+void DrawGameName();
+void FixConsoleWindow();
+void GotoXY(int coordX, int coordY);
+void DrawBox();
+
+#endif FUNCTION_H_
---
+++ MasterHeader.h
@@ -0,0 +1,22 @@
+#ifndef MASTERHEADER_H_
+#define MASTERHEADER_H_
+
+#include <iostream>
+#include <thread>
+#include <Windows.h>
+#include <processthreadsapi.h>
+#include <string>
+#include <conio.h>
+#include <vector>
+
+
+#define MAX_LEVEL 10
+#define MAX_BIRD 6
+#define MAX_DINOSAUR 6
+#define MAX_TRUCK 6
+#define MAX_CAR 6
+
+extern bool STOP_FLAG;
+extern bool FINISH_FLAG;
+
+#endif MASTERHEADER_H_
