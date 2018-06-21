4a14178223580d13ae7771eaae5d093acad4df4a
Tuan Pham <phamtuan199911@gmail.com>
2018-06-21 13:33:02 +0700

    change MasterHeader

--- MasterHeader.h
+++ MasterHeader.h
@@ -2,6 +2,8 @@
 #define MASTERHEADER_H_
 
 #include <iostream>
+#include <fstream>
+#include <iterator>
 #include <thread>
 #include <Windows.h>
 #include <processthreadsapi.h>
@@ -14,10 +16,10 @@
 #define MAX_DINOSAUR 6
 #define MAX_TRUCK 6
 #define MAX_CAR 6
-
 #define WIDTH 800
 #define HEIGHT 600
 #define SAVE_PATH "./save.dat"
+
 
 extern bool STOP_FLAG;
 extern bool FINISH_FLAG;
